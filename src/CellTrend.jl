a954da94a627b18ebe8e3f7723fc419ac6b16410
Steve Frank <saf@stevefrank.org>
2024-03-30 10:20:45 -0700

    Adjusted scaling of fluctuations so that system can respond to changes. Seems to fit so that system can track prior input value.

--- src/CellTrend.jl
+++ src/CellTrend.jl
@@ -10,8 +10,9 @@ export ema, rw, driver_opt
 
 sigmoidc(x; epsilon=1e-5) = clamp(1 / (1 + exp(-x)), epsilon, 1-epsilon)
 
+# globals for callback
 acc_ma = 0
-iter = 0
+iter = 0			
 
 function driver_opt(T=30; maxiters=10)
 	global acc_ma = 0.5
@@ -22,8 +23,8 @@ function driver_opt(T=30; maxiters=10)
 
 	tf, pp, st, re = make_activation()
 	nparam = 4*n + length(pp) + 4		# 4 is for location and scale of prediction
-	p = find_eq(u0, nparam, 10000, tf, st, re)
-	p[end] = u0[2*n]			# set so that u[8] = p[end], and guess is 0.5
+	p = find_eq(u0, nparam, tf, st, re)
+	p[end] = 1 / u0[2*n]			# set so that 1/u[8] = p[end], and guess is 0.5
 	optf = OptimizationFunction((p,x) -> loss(p,n,T,u0,tf,st,re; saveat=saveat),
 				Optimization.AutoForwardDiff())
 	prob = OptimizationProblem(optf,p)
@@ -32,7 +33,7 @@ function driver_opt(T=30; maxiters=10)
 					maxiters=maxiters, callback=callback)
 end
 
-function find_eq(u0, nparam, maxiters, tf, st, re)
+function find_eq(u0, nparam, tf, st, re; maxiters=10000)
 	n = 4
 	p = 1e0 * rand(nparam) .+ 0.1		# must adjust weighting for equil values
 	optf = OptimizationFunction((p,x) -> loss_eq(p,n,u0,tf,st,re))
@@ -81,16 +82,15 @@ end
 # parameters to be optimized, and u are the ode variables
 # Train so that p1 is input at last step, p2 is current input - prev,
 # with p1 and p2 both adjust by affine transformation of parameters
-function loss(p, n, T, u0, tf, st, re; saveat=0.1, skip=0.25)
-	skip = Int(floor(skip*T/saveat))
+function loss(p, n, T, u0, tf, st, re; saveat=0.1, skip=0.1, scale=1e4)
 	tspan = (0,T)
-	y,t = rw(T/10000; sigma=0.2, saveat=0.0001*saveat, low=0.25, high=0.75)
-	v = ema_interp(y,10000*t)
+	y,t = rw(T/scale; sigma=0.2, saveat=1/scale, low=0.4, high=0.6)
+	v = ema_interp(y,scale*t)
 	prob = ODEProblem((u,p,t) -> ode(u, p, t, n, v, tf, st, re), u0, tspan, p)
-	sol = solve(prob, Tsit5(), saveat=saveat, maxiters=100000)
+	sol = solve(prob, Tsit5(), saveat=1.0, maxiters=100000)
+	skip = Int(floor(skip*length(sol.t)))
 	y_diff = calc_y_diff(y,1)[1+skip:end]
 	y_true = calc_y_true(y_diff)
-	#s = @view sol[n+2,:][1+skip:end-1]
 	s = @view sol[n+1,:][1+skip:end]
 	yp = y_true
 	#return sum(abs2.(s .- p[end-1]*(y_diff .- p[end]))), yp, y_true, sol
@@ -135,7 +135,8 @@ function callback(state, loss, yp, y_true, sol, y, p)
 			plot!(sol[i,:],subplot=panels[i])
 		end
 		plot!(y[20:end-1], subplot=8,color=mma[1])
-		plot!(p[end-1]*(sol[5,21:end] .- p[end]), subplot=8,color=mma[2])
+		#plot!(p[end-1]*(sol[5,21:end] .- p[end]), subplot=8,color=mma[2])
+		plot!(0.5*p[end]*sol[5,21:end], subplot=8,color=mma[2])
 		display(pl)
 	end
 	return false
