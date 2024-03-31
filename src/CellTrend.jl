fd77bdfecedd56f65744840a4fa717c5fb907cac
Steve Frank <saf@stevefrank.org>
2024-03-31 11:43:48 -0700

    Fixed scaling, can set time between changes in direction.

--- src/CellTrend.jl
+++ src/CellTrend.jl
@@ -14,8 +14,8 @@ sigmoidc(x; epsilon=1e-5) = clamp(1 / (1 + exp(-x)), epsilon, 1-epsilon)
 acc_ma = 0
 iter = 0			
 
-function driver_opt(T=30; maxiters=10, save=false, saveit=0.1, n=4,
-		dir="/Users/steve/Desktop/")
+function driver_opt(T=30; maxiters=10, save=false, saveat=0.1, n=4,
+		dir="/Users/steve/Desktop/", learn=0.005, scale=1e1)
 	global acc_ma = 0.5
 	global iter = 0
 	u0 = vcat(1e0*ones(n),1e0*ones(n))
@@ -24,14 +24,15 @@ function driver_opt(T=30; maxiters=10, save=false, saveit=0.1, n=4,
 	nparam = 4*n + length(pp) + 6		# 6 is for location and scale of prediction
 	p = find_eq(u0, nparam, tf, st, re)
 	p[end] = u0[2*n]			# set so that u[8] = p[end]
-	optf = OptimizationFunction((p,x) -> loss(p,n,T,u0,tf,st,re; saveat=saveat),
+	optf = OptimizationFunction(
+				(p,x) -> loss(p,n,T,u0,tf,st,re; saveat=saveat,scale=scale),
 				Optimization.AutoForwardDiff())
 	prob = OptimizationProblem(optf,p)
 	# OptimizationOptimisers.Sophia(η=0.001)
 	s = solve(prob, OptimizationOptimisers.Sophia(η=0.005), # η=
 					maxiters=maxiters, callback=callback)
-	d = (p=s.u, loss=p.objective, tf=tf, re=re, st=st, n=n, T=T, maxiters=maxiters,
-			saveit=saveit, u0=u0)
+	d = (p=s.u, loss=s.objective[1], tf=tf, re=re, st=st, n=n, T=T, maxiters=maxiters,
+			saveat=saveat, u0=u0, learn=learn, scale=scale)
 	if save
 		date = Dates.format(Dates.now(), "yyyy-mm-dd-HH-MM-SS");
 		serialize(dir * "$date.jls", d);
@@ -62,6 +63,8 @@ end
 
 nn_input(pr,input) =
 	([pr[1],pr[2],input],[pr[1],pr[2],input],pr[1:2],[pr[3]])
+tf_out(tf,u_p,input,re,p_tf,st,n) = 
+	[tf(nn_input(u_p,input), re(p_tf), st)[1][i][1] for i in 1:n]
 
 # requires 49+x parameters, length(p) == 16 + 33 + x, where x is number used in loss
 function ode(u, p, t, n, v, tf, st, re)
@@ -75,10 +78,9 @@ function ode(u, p, t, n, v, tf, st, re)
 	p_tf = @view p[4n+1:end-6]
 	
 	input = v(t)
-		
-	f = [tf(nn_input(u_p,input), re(p_tf), st)[1][i][1] for i in 1:n]
+	f = tf_out(tf,u_p,input,re,p_tf,st,n)
 	
-	du_m = m_a .* f .- m_d .* u_m			# mRNA level
+	du_m = m_a .* f .- m_d .* u_m		# mRNA level
 	du_p = p_a .* u_m .- p_d .* u_p		# protein level
 	return vcat(du_m, du_p)
 end
@@ -88,23 +90,27 @@ end
 # parameters to be optimized, and u are the ode variables
 # Train so that p1 is input at last step, p2 is current input - prev,
 # with p1 and p2 both adjust by affine transformation of parameters
-function loss(p, n, T, u0, tf, st, re; saveat=0.1, skip=0.1, scale=1e4)
+function loss(p, n, T, u0, tf, st, re; saveat=0.1, skip=0.1, scale=1e1)
 	tspan = (0,T)
-	y,t = rw(T/scale; sigma=0.2, saveat=1/scale, low=0.25, high=0.75)
-	v = ema_interp(y,scale*t)
+	rescale = scale/saveat
+	y,t = rw(T/rescale; sigma=0.2, saveat=saveat, low=0.25, high=0.75)
+	v = ema_interp(y,rescale*t)
+	y = v.(0:T)
 	prob = ODEProblem((u,p,t) -> ode(u, p, t, n, v, tf, st, re), u0, tspan, p)
 	sol = solve(prob, Tsit5(), saveat=1.0, maxiters=100000)
 	skip = Int(floor(skip*length(sol.t)))
 	y_diff = calc_y_diff(y,1)[1+skip:end]
 	y_true = calc_y_true(y_diff)
 	s1 = p[end-1]*(sol[n+1,:][1+skip:end] .- p[end])
+	l1 = sum(abs2.(s1 .- y[skip:end-1]))
 	#s2 = p[end-3]*(sol[n+2,:][2+skip:end] .- p[end-2])
+	#l2 = sum(abs2.(s2 .- y_diff))	# check time matching
+	s2 = p[end-3]*(sol[n+2,:][1+skip:end] .- p[end-2])
+	l2 = sum(abs2.(s2 .- y[1+skip:end]))
 	s3 = p[end-5]*(sol[n+3,:][1+skip:end-1] .- p[end-4])
 	yp = sigmoidc.(s3)
-	l1 = sum(abs2.(s1 .- y[skip:end-1]))
-	#l2 = sum(abs2.(s2 .- y_diff))	# check time matching
 	lm = sum(CrossEntropyLoss(),yp,y_true)
-	return l1+lm, yp, y_true, sol, y, p
+	return l1+l2+lm, yp, y_true, sol, y, p
 end
 
 function loss_eq(p, n, u0, tf, st, re)
@@ -129,6 +135,8 @@ function callback(state, loss, yp, y_true, sol, y, p)
 		end
 		plot!(y[skip:end-1], subplot=9,color=mma[1])
 		plot!(p[end-1]*(sol[5,1+skip:end] .- p[end]), subplot=9,color=mma[2])
+		plot!(y[1+skip:end], subplot=10,color=mma[1])
+		plot!(p[end-3]*(sol[6,1+skip:end] .- p[end-2]), subplot=10,color=mma[2])
 		display(pl)
 	end
 	return false
--- src/Run.jl
+++ src/Run.jl
@@ -8,3 +8,11 @@ du = ones(8);
 CellTrend.ode!(du, rand(8), rand(30), 1, 4, v)
 
 y = CellTrend.rw(300);
+
+######################
+
+d = driver_opt(5000;maxiters=20, save=false, saveat=0.1, n=4,
+		dir="/Users/steve/Desktop/", learn=0.005);
+r=rand(d.n);
+CellTrend.tf_out(d.tf,r,7,d.re,d.p[17:49],d.st,d.n)
+
