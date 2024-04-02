5a8ee511467cfbb648ab52220fe27cf1c6241d74
Steve Frank <saf@stevefrank.org>
2024-04-02 07:24:37 -0700

    Successful match to diffs, with increased accuracy of prediction. Next, try longer T.

--- src/CellTrend.jl
+++ src/CellTrend.jl
@@ -1,7 +1,7 @@
 module CellTrend
 using DifferentialEquations, Optimization, LossFunctions, Statistics,
 		OptimizationOptimisers, OptimizationOptimJL, Lux, Random, Plots,
-		Printf, Dates
+		Printf, Dates, Serialization
 include("Data.jl")
 using .Data
 include("/Users/steve/sim/zzOtherLang/julia/modules/MMAColors.jl")
@@ -14,7 +14,7 @@ sigmoidc(x; epsilon=1e-5) = clamp(1 / (1 + exp(-x)), epsilon, 1-epsilon)
 acc_ma = 0
 iter = 0			
 
-function driver_opt(T=30; maxiters=10, save=false, saveat=0.1, n=3,
+function driver_opt(T=30; maxiters=10, save=true, saveat=0.1, n=3,
 		dir="/Users/steve/Desktop/", learn=0.005, scale=1e1)
 	global acc_ma = 0.5
 	global iter = 0
@@ -33,11 +33,13 @@ function driver_opt(T=30; maxiters=10, save=false, saveat=0.1, n=3,
 					maxiters=maxiters, callback=callback)
 	d = (p=s.u, loss=s.objective[1], tf=tf, re=re, st=st, n=n, T=T, maxiters=maxiters,
 			saveat=saveat, u0=u0, learn=learn, scale=scale)
-	if save
-		date = Dates.format(Dates.now(), "yyyy-mm-dd-HH-MM-SS");
-		serialize(dir * "$date.jls", d);
-	end
+	if save save_results(d; dir=dir) end
 	return d
+end
+
+function save_results(d; dir="/Users/steve/Desktop/")
+	date = Dates.format(Dates.now(), "yyyy-mm-dd-HH-MM-SS");
+	serialize(dir * "$date.jls", d);	
 end
 
 function find_eq(n, u0, nparam, tf, st, re; maxiters=10000)
@@ -52,17 +54,20 @@ end
 function make_activation()
 	f1 = Chain(Dense(3 => 2, mish), Dense(2 => 1))
 	f2 = Chain(Dense(3 => 2, mish), Dense(2 => 1))
-	f3 = Chain(Dense(3 => 2, mish), Dense(2 => 1))
+	f3 = Chain(Dense(2 => 2, mish), Dense(2 => 1))
 	f = Parallel(nothing, f1, f2, f3)
 	ps, st = Lux.setup(Random.default_rng(),f)
 	pp, re = destructure(ps)
 	return f, pp, st, re
 end
 
-nn_input(pr,input) =
-	([pr[1],pr[2],input],[pr[1],pr[2],input],[pr[1],pr[2],input])
 tf_out(tf,u_p,input,re,p_tf,st,n) = 
 	[tf(nn_input(u_p,input), re(p_tf), st)[1][i][1] for i in 1:n]
+
+function nn_input(pr,input)
+	x = [pr[1],pr[2],input]
+	return (x,x,pr[1:2])
+end
 
 # length(p) == 4n + p_nn + x, where x is number used in loss
 function ode(u, p, t, n, v, tf, st, re)
@@ -100,14 +105,14 @@ function loss(p, n, T, u0, tf, st, re; saveat=0.1, skip=0.1, scale=1e1)
 	y_true = calc_y_true(y_diff)
 	s1 = p[end-1]*(sol[n+1,:][1+skip:end] .- p[end])
 	l1 = sum(abs2.(s1 .- y[skip:end-1]))
-	#s2 = p[end-3]*(sol[n+2,:][2+skip:end] .- p[end-2])
-	#l2 = sum(abs2.(s2 .- y_diff))	# check time matching
 	s2 = p[end-3]*(sol[n+2,:][1+skip:end] .- p[end-2])
 	l2 = sum(abs2.(s2 .- y[1+skip:end]))
 	s3 = p[end-5]*(sol[n+3,:][1+skip:end-1] .- p[end-4])
 	yp = sigmoidc.(s3)
-	lm = sum(CrossEntropyLoss(),yp,y_true)
-	return l1+l2+lm, yp, y_true, sol, y, p, n
+	#lm = sum(CrossEntropyLoss(),yp,y_true)
+	s3_2 = p[end-5]*(sol[n+3,:][2+skip:end] .- p[end-4])
+	l3_2 = 10*sum(abs2.(s3_2 .- y_diff))
+	return l1+l2+l3_2, yp, y_true, sol, y, p, n, y_diff
 end
 
 function loss_eq(p, n, u0, tf, st, re)
@@ -116,7 +121,7 @@ function loss_eq(p, n, u0, tf, st, re)
 	return sum(abs2.(du))
 end
 
-function callback(state, loss, yp, y_true, sol, y, p, n)
+function callback(state, loss, yp, y_true, sol, y, p, n, y_diff)
 	global iter += 1
 	acc = accuracy(yp,y_true)
 	pred_pos = sum((yp .> 0.5) .== true) / length(yp)
@@ -124,7 +129,7 @@ function callback(state, loss, yp, y_true, sol, y, p, n)
 	if iter % 10 == 0
 		@printf("%4d: Loss = %8.2f, accuracy = %5.3f, fr_pos = %5.3f\n",
 			iter, loss, acc_ma, pred_pos)
-		pl = plot(layout=(n+1,2),size=(800,150*(n+1)),legend=:none)
+		pl = plot(layout=(n+2,2),size=(800,150*(n+2)),legend=:none)
 		panels = vcat(1:2:(2n-1),2:2:2n)
 		skip = Int(floor(0.1*length(y)))
 		for i in 1:2n
@@ -134,6 +139,10 @@ function callback(state, loss, yp, y_true, sol, y, p, n)
 		plot!(p[end-1]*(sol[n+1,1+skip:end] .- p[end]), subplot=2n+1,color=mma[2])
 		plot!(y[1+skip:end], subplot=2n+2,color=mma[1])
 		plot!(p[end-3]*(sol[n+2,1+skip:end] .- p[end-2]), subplot=2n+2,color=mma[2])
+		#plot!(y[skip:end-1], subplot=2n+3,color=mma[1])
+		plot!(y_diff, subplot=2n+3,color=mma[2])
+		plot!(p[end-5]*(sol[n+3,:][2+skip:end] .- p[end-4]),
+				subplot=2n+3,color=mma[3],w=0.5)
 		display(pl)
 	end
 	return false
