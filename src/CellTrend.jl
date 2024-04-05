e3af1fb61d0c6369e7299d48d6c1515585327c25
Steve Frank <saf@stevefrank.org>
2024-04-05 09:16:35 -0700

    Much simplified system works well. Refine.

--- src/CellTrend.jl
+++ src/CellTrend.jl
@@ -15,13 +15,12 @@ acc_ma = 0
 iter = 0			
 
 function driver_opt(T=30; maxiters=10, save=true, saveat=0.1, n=3,
-		dir="/Users/steve/Desktop/", learn=0.005, scale=1e1,
-		restart="", loss_type="12")
+		dir="/Users/steve/Desktop/", learn=0.005, scale=1e1, restart="")
 	global acc_ma = 0.5
 	global iter = 0
 	u0 = [1,1,0.5]
 
-	nparam = 11 + 6		# 6 is 2 for each of 3 variables
+	nparam = 6 + 1
 	if restart !== ""
 		d = deserialize(dir * restart)
 		p = d.p
@@ -29,8 +28,7 @@ function driver_opt(T=30; maxiters=10, save=true, saveat=0.1, n=3,
 		p = find_eq(u0, nparam)
 	end
 	optf = OptimizationFunction(
-				(p,x) -> loss(p,T,u0;
-					loss_type=loss_type, saveat=saveat,scale=scale),
+				(p,x) -> loss(p,T,u0; saveat=saveat,scale=scale),
 				Optimization.AutoForwardDiff())
 	prob = OptimizationProblem(optf,p)
 	# OptimizationOptimisers.Sophia(η=0.001)
@@ -61,9 +59,9 @@ function find_eq(u0, nparam; maxiters=10000)
 end
 
 function ode(u, p, t, v)
-	du1 = p[1]*v(t) - p[2]*u[1] + p[10]*u[2]
-	du2 = p[3]*v(t) - p[4]*u[2] + p[11]*u[1]
-	du3 = p[5]*(u[1]/(p[6]+u[1])) - p[7]*(u[2]/(p[8]+u[2])) - p[9]*u[3]
+	du1 = p[1]*v(t) - p[2]*u[1]
+	du2 = p[3]*(p[1]*v(t) - p[2]*u[2])	# faster response, same equil
+	du3 = p[4] + p[5]*(u[2] - u[1]) - p[6]*u[3]
 	return [du1,du2,du3]
 end
 
@@ -72,7 +70,7 @@ end
 # parameters to be optimized, and u are the ode variables
 # Train so that p1 is input at last step, p2 is current input - prev,
 # with p1 and p2 both adjust by affine transformation of parameters
-function loss(p, T, u0; loss_type="12", saveat=0.1, skip=0.1, scale=1e1)
+function loss(p, T, u0; saveat=0.1, skip=0.1, scale=1e1)
 	tspan = (0,T)
 	rescale = scale/saveat
 	y,t = rw(T/rescale; sigma=0.2, saveat=saveat, low=0.25, high=0.75)
@@ -84,23 +82,13 @@ function loss(p, T, u0; loss_type="12", saveat=0.1, skip=0.1, scale=1e1)
 	skip = Int(floor(skip*length(sol.t)))
 	y_diff = calc_y_diff(y,1)[1+skip:end]
 	y_true = calc_y_true(y_diff)
-	s1 = p[end]*(sol[1,:][1+skip:end] .- p[end-1])
-	s2 = p[end-2]*(sol[2,:][1+skip:end] .- p[end-3])
-	s3 = p[end-4]*(sol[3,:][1+skip:end] .- 0.5)
+	s1 = sol[1,:][1+skip:end]
+	s2 = sol[2,:][1+skip:end]
+	s3 = p[end]*(sol[3,:][1+skip:end] .- p[4]/p[6])
 	s = [s1,s2,s3]
-	yp = sigmoidc.(s3 .- p[end-5])[1:end-1]
-	lt = 0
-	if loss_type == "12" || loss_type == "all"
-		l1 = sum(abs2.(s1 .- y[skip:end-1]))
-		l2 = sum(abs2.(s2 .- y[1+skip:end]))
-		lt = l1+l2
-		if loss_type == "all" lt /= 100 end
-	end
-	if loss_type == "3" || loss_type == "all"
-		lm = sum(CrossEntropyLoss(),yp,y_true)
-		lt += lm
-	end
-	return lt, yp, y_true, s, y, p, y_diff, skip
+	yp = sigmoidc.(s3)[1:end-1]
+	lm = sum(CrossEntropyLoss(),yp,y_true)
+	return lm, yp, y_true, s, y, p, y_diff, skip
 end
 
 function loss_eq(p, u0)
@@ -119,14 +107,9 @@ function callback(state, loss, yp, y_true, s, y, p, y_diff, skip; direct=false)
 		@printf("%4d: Loss = %8.2e, accuracy = %5.3f, max_acc = %5.3f, fr_pos = %5.3f\n",
 			iter, loss, acc_ma, max_acc, pred_pos)
 		pl = plot(layout=(3,1),size=(800,900),legend=:none)
-		plot!([y[skip:end-1],s[1]],subplot=1)
-		plot!([y[1+skip:end],s[2]],subplot=2)
-		plot!([y[1+skip:end],s[3]],subplot=3)
-		
-# 		plot!(y_diff, subplot=2n+3,color=mma[1])
-# 		ypc = yp .- 0.5
-# 		ypm = maximum(abs.(ypc))
-# 		plot!(ypc ./ (30*ypm), subplot=2n+3,color=mma[2])
+		plot!(y[1+skip:end],subplot=1)
+		plot!([s[1],s[2]],subplot=2)
+		plot!(s[3],subplot=3)
 		display(pl)
 	end
 	return false
